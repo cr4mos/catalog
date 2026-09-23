76fcec4a321903a00118e1c94a690cb7687eb9ab
Carlos Ramos <carlos.930919@gmail.com>
2026-09-23 12:53:15 -0600

    feat(unit-test): unit test configuration added

---
+++ productsCatalogTestTests/productsCatalogTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  productsCatalogTestTests.swift
+//  productsCatalogTestTests
+//
+//  Created by Carlos Ramos on 23/09/26.
+//
+
+import Testing
+@testable import productsCatalogTest
+
+struct ProductsCatalogTests {
+
+    @Test
+    func example() {
+        #expect(1 + 1 == 2)
+    }
+}
